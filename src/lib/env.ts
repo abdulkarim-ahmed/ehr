f54e4c7e2b32e8332171f95339c90baeb7bf414c
Abdulkarim Ahmed <abdulkarimahmedak@gmail.com>
2025-08-26 07:45:09 +0300

    feat: add altUae environment variable for iframe URL configuration

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,5 +2,6 @@ export const ENVS = {
   alt: import.meta.env.VITE_IFRAME_URL_ALT,
   dev: import.meta.env.VITE_IFRAME_URL_DEV,
   altProd: import.meta.env.VITE_IFRAME_URL_ALTPROD,
+  altUae: import.meta.env.VITE_IFRAME_URL_ALTUAE,
   prod: import.meta.env.VITE_IFRAME_URL_PROD
 }
